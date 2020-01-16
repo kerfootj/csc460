a8b214467978bc651433f3971e48f53f69959ea2
Joel Kerfoot <joel.kerfoot@outlook.com>
2020-01-15 20:23:23 -0800

    Update exercise_1.ino

--- exercise1/exercise_1.ino
+++ exercise1/exercise_1.ino
@@ -1,38 +1,39 @@
-int eof = 35; // #
 int count = 0;
-int total;
+int total = 0;
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial1.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if(Serial.available() > 0) {
     Serial1.write(Serial.read());
   }
+  
   if(Serial1.available() > 0) {
-    
-    if (count > 0 && count % 30 == 0) {
-      Serial.println();
-    }
 
     int incoming = Serial1.read();
 
-    if (incoming == eof) {
+    // eof display count and exit
+    if (incoming == '#') {
       Serial.print("Total bytes sent/recieved: ");
       Serial.println(total);
       delay(100);
       exit(0);
-    } else if (incoming == '\n') {
-      count = 0;
-    } else {
-      count += 1;
     }
+
+    // create new line when longer than 30 bytes
+    if (count > 0 && count % 30 == 0) {
+      Serial.println();
+    }
+
+    // increment total and adjust count
     total += 1;
+    count = (incoming == '\n') ? 0 : count + 1;
+
+    // write the byte
     Serial.write(incoming);
   }
 }
